841bca35a8a15a121496b02c6aad0c323ae9f162
Mohammed Ayan <112795104+AyanChaudhary@users.noreply.github.com>
2023-06-17 13:08:35 +0530

    Update Constants.kt

--- app/src/main/java/com/example/weatherapp2/others/Constants.kt
+++ app/src/main/java/com/example/weatherapp2/others/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.weatherapp2.others
 
 object Constants {
-    const val API_KEY="89d7041aea6c8aabdc2dbf6529f43a3c"
+    const val API_KEY=""
     const val BASE_URL="https://api.openweathermap.org/data/2.5/"
     const val PERMISSION_REQUEST_CODE=100
 
